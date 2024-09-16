57b48f85c92c1ff985d29eff0d6807e2347389a1
Scott Kirker <scott.kirker@gmail.com>
2024-09-16 16:08:48 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# Create a game that does the following:
+
+# 1. Shows the name, handle, and bio of two randomly selected influencers from the dataset.
+
+# 2. Takes user input to guess which of the two influencers has more followers.
+
+# 3. Evaluates which influencer has more followers and compares the correct answer to the user's guess.
+
+# 4. Upon a correct guess, brings up a new random influencer to compare to the "winner" of the prior round.
+
+# 5. Upon an incorrect guess, selects two entirely new influencers at random from the remaining options.
+
+# 6. Keeps track of how many times the user guesses incorrectly.
+
+# 7. Ends the game after 5 incorrect guesses.
+
+# 8. Ensures no "losing" influencer is offered to the user more than once.
